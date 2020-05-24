adb9bd5bbcdb72a670aae7ab9027deb078bde485
Ziyen <zyloh89@gmail.com>
2020-05-24 23:11:46 +0930

    add files

---
+++ index.js
@@ -0,0 +1,230 @@
+const util = require("util");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+//Connection
+
+const connection = mysql.createConnection({ 
+    host: 'localhost',
+    user: 'root',
+    password: 'Iamadog89!', //DELETE
+    database: 'employees_db'
+ });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('Connected as id ' + connection.threadId);
+    console.log("Welcome to Employee Tracker");
+    start();
+});
+
+connection.query = util.promisify(connection.query);
+
+//Prompt
+
+function start() {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'mainpage',
+            message: "What would you like to do to the employee database?",
+            choices: [
+                "Add departments, roles, employees",
+                "View departments, roles, employees",
+                "Update employee roles"
+            ]
+    }).then(function(result) {
+        if(result.mainpage === "Add departments, roles, employees") {
+            addFunction();
+        }
+        else if(result.mainpage === "View departments, roles, employees") {
+            viewFunction();
+        }
+        else if(result.mainpage === "Update employee roles") {
+            updateFunction();
+        }
+        else {
+            console.log("Exit")
+            connection.end();
+        }
+    });
+};
+
+// Add Function
+
+function addFunction() {
+    inquirer.prompt(
+        {
+            type: "list",
+            name: "addfunction",
+            message: "What would you like to add to the database?",
+            choices: [
+                "department",
+                "role",
+                "employee",
+                "cancel"
+            ]
+        }).then(function(result) {
+            if(result.addfunction === "department") {
+                inquirer.prompt(
+                    {
+                        type: "input",
+                        name: "department_name",
+                        message: "What is the department name?"
+                    }).then(function(result) {
+                        addDepartment(result.department_name);
+                    });
+            }
+            else if (result.addfunction === "role") {
+                inquirer.prompt([            
+                    {
+                        type: "input",
+                        name: "title",
+                        message: "What is the title for the role?"
+                    },
+                    {
+                        type: "input",
+                        name: "salary",
+                        message: "What is the salary for the role?"
+                    },
+                    {
+                        type: "input",
+                        name: "department",
+                        message: "Which department does the role fall into? Please enter department id."
+                    }
+                ]).then(function(result) {
+                    addRole(result.title, result.salary, result.department);
+                })
+            }
+            else if (result.addfunction === "employee") {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "firstname",
+                        message: "What is the first name of the employee?"
+                    },
+                    {
+                        type: "input",
+                        name: "lastname",
+                        message: "What is the last name of the employee?"
+                    },
+                    {
+                        type: "input",
+                        name: "role",
+                        message: "What is the role of the employee? Please enter role id."
+                    },
+                    {
+                        type: "input",
+                        name: "manager",
+                        message: "Who is the manager of the employee? Please enter manager id, if required."
+                    }
+                ]).then(function(result) {
+                    addEmployee(result.firstname, result.lastname, result.role, result.manager);
+                    });
+            } else {
+                start(); 
+            }      
+        });
+}
+
+// Update Function
+
+function updateFunction() {
+    inquirer.prompt({
+        type: "list",
+        name: "updateDb",
+        message: "What would you like to update?",
+        choices: ["role", "cancel update"]
+    }).then(function (result) {
+        if (answer.updateDb === "cancel update") {
+            start();
+        }
+        else {
+            connection.query("SELECT * FROM employee", function (err, data) {
+                if (err) throw err;
+                if (answer.updateDb === "role") {
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "choice",
+                            message:  "Which employee id needs to be updated?",
+                            choices: renderEmployeeId(data)
+                        },
+                        {
+                            type: "input",
+                            name: "role",
+                            message: "What is the new role of the employee? Please enter new role id."
+                        }
+                    ]).then(function (result) {
+                        let newRoleId = setNewRoleId(data, result);
+                        updateRole(data.role, newRoleId.id);
+                    });
+                }
+            })
+        }
+    })
+}
+
+// Employee Id Array
+function renderEmployeeId (result) {
+    let employeeIdArray = [];
+    for (var i = 0; i < result.length; i++) {
+        employeeIdArray.push(result[i].id);
+    }
+    return employeeIdArray;
+}
+
+// Update new employee role ID
+function setNewRoleId(data, result) {
+    let newRoleId;
+    for (var i = 0; i < result.length; i++) {
+        if (result[i].id === data.choice) {
+            newRoleId = result[i];
+        }
+    }
+    return newRoleId;
+}
+
+// Add Department Query
+
+function addDepartment(name) {
+    connection.query("INSERT INTO department SET ?", 
+    {
+        name: name
+    },
+    function (err) {
+        if (err) throw err;
+        console.log(name + "department added to database");
+        start();
+    });
+}
+
+// Add Role Query
+function addRole(title, salary, department) {
+    const role = {
+        title: title,
+        salary: salary,
+        department_id: department
+    }
+    connection.query("INSERT INTO role SET ?", role, function (err) {
+        if (err) throw err;
+        console.log(title + " role added to database");
+    });
+}
+
+
+// Add Employee Query
+function addEmployee (firstname, lastname, role, manager) {
+    const employee = {
+        first_name: firstname,
+        last_name: lastname,
+        role_id: role,
+        manager_id: manager
+    }
+    connection.query("INSERT INTO employee SET ?", employee, function (err) {
+        if (err) throw err;
+        console.log(firstname + " " + lastname + " new employee added to database")
+    })
+
+}
--- seed.sql
+++ seed.sql
@@ -1,10 +1,10 @@
 DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employeess_db;
+CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
   id INT AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(30), -- dept name
+  department_name VARCHAR(30) NOT NULL, -- dept name
   PRIMARY KEY (id)
 );
 
@@ -27,3 +27,20 @@ CREATE TABLE employee(
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department(department_name) VALUES ('Audit');
+INSERT INTO department(department_name) VALUES ('Tax');
+INSERT INTO department(department_name) VALUES ('Marketing');
+INSERT INTO department(department_name) VALUES ('Admin');
+
+INSERT INTO role(title, salary, department_id) VALUES ('Manager', 100000, 1);
+INSERT INTO role(title, salary, department_id) VALUES ('Accountant', 80000, 2);
+INSERT INTO role(title, salary, department_id) VALUES ('Intern', 40000, 3);
+INSERT INTO role(title, salary, department_id) VALUES ('Payroll Officer', 60000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Madhavi', 'Murali', 1, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Andrew', 'Harris', 2, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Anna', 'Lee', 3, 1);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Richard', 'Davis', 4, 4);
+
+SELECT * FROM employee;
